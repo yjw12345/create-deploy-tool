41bf21a0fe686f192d161d1e9137237d905b1496
yjw12345 <1262793484@qq.com>
2024-04-03 12:24:03 +0800

    feat: add output

--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
 import ejs from 'ejs'
 import * as fs from 'node:fs'
-import { fileURLToPath } from 'node:url'
-import { dirname, resolve } from 'node:path'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import * as path from 'node:path'
 
-console.log(__dirname)
-
-const pushEjsPath = resolve(__dirname, './template/ssh/push.ejs')
-console.log(pushEjsPath)
-const pushEjsTemplate = fs.readFileSync(pushEjsPath, 'utf-8')
+const pushEjsPath = path.resolve(__dirname, './template/ssh/push.ejs')
 ejs.renderFile(pushEjsPath, { filePath: 'hello' }, null, (_, str) => {
-  fs.writeFileSync(resolve(__dirname, './output.js'), str)
+  fs.writeFileSync(path.resolve(__dirname, './out/push.js'), str)
 })
