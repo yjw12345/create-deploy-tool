d58e8cba8bf717c7ed6b5b05475ee21a078f9a0d
yjw12345 <1262793484@qq.com>
2024-04-04 16:47:18 +0800

    feat: git

--- scripts/commit.js
+++ scripts/commit.js
@@ -1,20 +1,31 @@
-import execa from 'execa'
-import { $ } from 'zx'
-const projectRoot = process.cwd()
+const { exec } = require('child_process'); // 引入child_process模块
+const readline = require('readline');
 
-execa.commandSync("git add .", {
-    stdio: "inherit",
-    cwd: projectRoot,
+// 编辑Git提交内容
+// 创建readline接口实例
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
-setTimeout(() => {
-    execa.commandSync(`git commit -m 'auto commit'`, {
-        stdio: "inherit",
-        cwd: projectRoot,
+const editGitCommit = () => {
+    // 调用接口方法
+    r1.question('输入提交的内容\t', (inpt) => {
+        console.log('提交中......');
+        exec(`git add . && git commit --no-verify -m "${inpt}" && git push`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`执行命令时出错： ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`命令执行错误： ${stderr}`);
+                return;
+            }
+            console.log(`Git 提交成功： ${stdout}`);
+            // 不加close，则不会结束question
+            r1.close();
+            // 结束所有进程
+            end();
+        });
     });
-}, 3000);
-setTimeout(() => {
-    execa.commandSync(`git push`, {
-        stdio: "inherit",
-        cwd: projectRoot,
-    });
-}, 6000);
+};
+editGitCommit()
