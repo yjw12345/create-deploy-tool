55d36485d0efa85d15d9117363127cd416f14d3b
yjw12345 <1262793484@qq.com>
2024-04-04 10:34:49 +0800

    feat: add bin line

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
+
 import ejs from 'ejs'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import prompts from 'prompts'
 import deepMerge from './utils'
 import { green, bold } from 'kolorist'
+import execa from 'execa'
 async function init() {
+  // 获取用户输入，获取结果
   let result: ServeInfomation = {}
   result = await prompts([
     {
@@ -28,31 +32,38 @@ async function init() {
       message: '服务器部署目录',
     },
   ])
-  const pushEjsPath = path.resolve(__dirname, './template/ssh/push.ejs')
+  // 获取项目运行地址
+  const projectRoot = process.cwd()
+  const templateRoot = __dirname
+
+  const pushEjsPath = path.resolve(templateRoot, './template/ssh/push.ejs')
   // 如果没有这个目录的话就创建
-  const dir = path.resolve(__dirname, './config')
+  const dir = path.resolve(projectRoot, './config')
   if (!fs.existsSync(dir)) {
-    console.log('该路径已存在');
     fs.mkdirSync(dir);
   }
   // 使用ejs渲染ejs文件
   ejs.renderFile(pushEjsPath, result, null, (_, str) => {
-    fs.writeFileSync(path.resolve(__dirname, './config/push.js'), str, {})
+    fs.writeFileSync(path.resolve(projectRoot, './config/push.js'), str, {})
   })
   // 复制zip.js文件
-  const pushZipPath = path.resolve(__dirname, './template/ssh/zip.js')
+  const pushZipPath = path.resolve(templateRoot, './template/ssh/zip.js')
   const pushZipStr = fs.readFileSync(pushZipPath)
-  fs.writeFileSync(path.resolve(__dirname, './config/zip.js'), pushZipStr)
+  fs.writeFileSync(path.resolve(projectRoot, './config/zip.js'), pushZipStr)
   // 更新package.json
-  const packageJSONPath = path.resolve(__dirname, './template/ssh/package.json',)
+  const packageJSONPath = path.resolve(templateRoot, './template/ssh/package.json',)
   const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'))
-  const oldPackageJsonPath = path.resolve(__dirname, './package.json')
+  const oldPackageJsonPath = path.resolve(projectRoot, './package.json')
   const oldPackageJson = JSON.parse(fs.readFileSync(oldPackageJsonPath, 'utf-8'))
 
   deepMerge(oldPackageJson, packageJSON)
   fs.writeFileSync(oldPackageJsonPath, JSON.stringify(oldPackageJson, null, 2) + '\n')
 
-  console.log(`  ${bold(green('请使用npm install重新安装依赖'))}`)
+  console.log(`${bold(green('正在使用npm install重新安装依赖'))}`)
+  execa.commandSync("npm install", {
+    stdio: "inherit",
+    cwd: projectRoot,
+  });
 }
 init()
 
