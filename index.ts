ea050a47ae8088512f3d4ec74c21a35d4ce887c2
yjw12345 <1262793484@qq.com>
2024-04-06 12:23:54 +0800

    feat: unwrap index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import ejs from 'ejs'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import prompts from 'prompts'
-import deepMerge from './utils'
 import { green, bold } from 'kolorist'
 import execa from 'execa'
+import { renderTemplate } from './utils/renderTemplate'
+
 async function init() {
   // 获取用户输入，获取结果
   let result: ServeInfomation = {}
@@ -37,40 +37,26 @@ async function init() {
       message: '是否需要git自动提交',
     },
   ])
-  // 获取项目运行地址
+  // 获取项目运行地址，和脚本模版地址
   const projectRoot = process.cwd()
-  const templateRoot = __dirname
-
-  const pushEjsPath = path.resolve(templateRoot, './template/ssh/push.ejs')
-  // 如果没有这个目录的话就创建
+  const templateRoot = path.resolve(__dirname, './template')
+  // 创建scripts目录
   const dir = path.resolve(projectRoot, './scripts')
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-  // 使用ejs渲染ejs文件
-  ejs.renderFile(pushEjsPath, result, null, (_, str) => {
-    fs.writeFileSync(path.resolve(projectRoot, './scripts/push.js'), str, {})
-  })
-  // 复制zip.js文件
-  const pushZipPath = path.resolve(templateRoot, './template/ssh/zip.js')
-  const pushZipStr = fs.readFileSync(pushZipPath)
-  fs.writeFileSync(path.resolve(projectRoot, './scripts/zip.js'), pushZipStr)
-  // 更新package.json
-  const packageJSONPath = path.resolve(templateRoot, './template/ssh/package.json',)
-  const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'))
-  const oldPackageJsonPath = path.resolve(projectRoot, './package.json')
-  const oldPackageJson = JSON.parse(fs.readFileSync(oldPackageJsonPath, 'utf-8'))
 
-  deepMerge(oldPackageJson, packageJSON)
-  fs.writeFileSync(oldPackageJsonPath, JSON.stringify(oldPackageJson, null, 2) + '\n')
-  // 复制git
+  const render = function render(templateName: string) {
+    const templateDir = path.resolve(templateRoot, templateName)
+    renderTemplate(templateDir, projectRoot, result)
+  }
 
-  // 复制zip.js文件
-  const pushGitCommitPath = path.resolve(templateRoot, './template/git/commit.cjs')
-  const pushGitCommitStr = fs.readFileSync(pushGitCommitPath)
-  fs.writeFileSync(path.resolve(projectRoot, './scripts/commit.cjs'), pushGitCommitStr)
-  // 更新package.json
-  mergePackageJSON(path.resolve(templateRoot, './template/git/package.json'), path.resolve(projectRoot, './package.json'))
+  // 根据交互得到的数据进行克隆
+  render('ssh')
+  if (result.commit) {
+    render('git')
+  }
+
   // 更新依赖
   console.log(`${bold(green('正在使用npm install重新安装依赖'))}`)
   execa.commandSync("npm install", {
@@ -80,11 +66,3 @@ async function init() {
 }
 init()
 
-const mergePackageJSON = (templateDest: string, projectDest: string) => {
-  const existFlag = fs.existsSync(templateDest)
-
-  const templatePackageJson = existFlag ? JSON.parse(fs.readFileSync(templateDest, 'utf-8')) : {}
-  const projectPackageJson = JSON.parse(fs.readFileSync(projectDest, 'utf-8'))
-  deepMerge(templatePackageJson, projectPackageJson)
-  fs.writeFileSync(projectDest, JSON.stringify(projectPackageJson, null, 2) + '\n')
-}
---
+++ utils/renderTemplate.ts
@@ -0,0 +1,36 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import deepMerge from './deepMerge'
+import sortDependencies from './sortDependencies'
+import ejs from 'ejs'
+// src是模版文件，dest是目标文件，serverInformation是用户输入的值
+export const renderTemplate = (src: string, dest: string, serverInformation: ServeInfomation) => {
+    const stats = fs.statSync(src)
+    // 对目录下的文件进行遍历
+    if (stats.isDirectory()) {
+        if (path.basename(src) === 'node_modules') {
+            return
+        }
+        fs.mkdirSync(dest, { recursive: true })
+        for (const file of fs.readdirSync(src)) {
+            renderTemplate(path.resolve(src, file), path.resolve(dest, file), serverInformation)
+        }
+        return
+    }
+
+    const filename = path.basename(src)
+    if (filename === 'package.json' && fs.existsSync(dest)) {
+        // 将package.json对象合并
+        const existing = JSON.parse(fs.readFileSync(dest, 'utf8'))
+        const newPackage = JSON.parse(fs.readFileSync(src, 'utf8'))
+        const pkg = sortDependencies(deepMerge(existing, newPackage))
+        fs.writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n')
+        return
+    }
+    if (filename.endsWith('.ejs')) {
+        ejs.renderFile(src, serverInformation, null, (_, str) => {
+            fs.writeFileSync(dest, str, {})
+        })
+    }
+    fs.copyFileSync(src, dest)
+}
